41edbc590a21c6f408f4cca5e8e7e88d03f66a35
Abdi Deresa <Abdishegute@gmail.com>
2024-08-27 10:12:28 -0700

    added a next turn function

--- file.py
+++ file.py
@@ -1,8 +1,33 @@
 from tkinter import *
 import random
 
-def next_turn():
-    pass
+def next_turn(row, column):
+    global player
+    
+    if buttons[row][column]['text']=="" and check_winner() is False:
+        if player == players[0]:
+            buttons[row][column]['text']=player
+            
+            if check_winner() is False:
+                player = players[1]
+                label.config(text=(players[1]+" turn"))
+                
+            elif check_winner() is True:
+                label.config(text=(players[0]+" wins"))
+            elif check_winner()=="Tie":
+                label.config(text=("Tie"))
+        else:
+            buttons[row][column]['text']=player
+            
+            if check_winner() is False:
+                player = players[0]
+                label.config(text=(players[0]+" turn"))
+                
+            elif check_winner() is True:
+                label.config(text=(players[1]+" wins"))
+            elif check_winner()=="Tie":
+                label.config(text=("Tie"))
+                
 
 def check_winner():
     pass
@@ -35,5 +60,7 @@ for row in range(3):
     for column in range(3):
         buttons[row][column] = Button(frame, text="", font=('consolas',40), width=5, height=2,
                                      command=lambda row=row, column=column: next_turn(row, column) )
+        
+        buttons[row][column].grid(row=row, column = column)
 
 window.mainloop()
